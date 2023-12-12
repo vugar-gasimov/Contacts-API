4130811551c97ba919fc302db53e70e704f3d173
vugar-gasimov <vuqar5qasimov@gmail.com>
2023-12-12 21:16:52 +0200

    models, routes\api changed from basic to link contacts

--- models/contacts.js
+++ models/contacts.js
@@ -1,12 +1,9 @@
 //const fs = require('fs/promises')
 
 const listContacts = async () => {};
-
 const addContact = async (body) => {};
 const getContactById = async (contactId) => {};
-
 const removeContactById = async (contactId) => {};
-
 const updateContactById = async (contactId, body) => {};
 
 module.exports = {

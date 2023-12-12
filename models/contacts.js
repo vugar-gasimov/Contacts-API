0c60b980b1cb1ec33399919119b1719580e73a5e
Vugar.Gasimov <138323997+vugar-gasimov@users.noreply.github.com>
2023-12-12 17:42:09 +0200

    Initial commit

---
+++ models/contacts.js
@@ -0,0 +1,17 @@
+//const fs = require('fs/promises')
+const listContacts = async () => {};
+
+const addContact = async (body) => {};
+const getContactById = async (contactId) => {};
+
+const removeContactById = async (contactId) => {};
+
+const updateContactById = async (contactId, body) => {};
+
+module.exports = {
+  listContacts,
+  addContact,
+  getContactById,
+  removeContactById,
+  updateContactById,
+};

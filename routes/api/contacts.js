e1a355064b111eeda064ad777a61e5fa2524045f
vugar-gasimov <vuqar5qasimov@gmail.com>
2023-12-14 02:58:55 +0200

    app, +helpers,+ custom error js, contacts js, json package + joi and nanoid 334 routes/api/contacts js,  yarn lock

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,26 +1,76 @@
 const express = require("express");
+const Joi = require("joi");
 const contacts = require("../../models/contacts");
 const router = express.Router();
+const { CustomError } = require("../../helpers");
+
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+});
 
 router.get("/", async (req, res, next) => {
-  const result = await contacts.listContacts();
-  res.json(result);
+  try {
+    const result = await contacts.getAllContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 router.post("/", async (req, res, next) => {
-  const result = await contacts.addContact();
-  res.json(result);
+  try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      throw CustomError(400, error.message);
+    }
+    const result = await contacts.addAContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
 });
-router.get("/:contactId", async (req, res, next) => {
-  const result = await contacts.getContactById();
-  res.json(result);
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contacts.getContactById(id);
+    if (!result) {
+      throw CustomError(404, "Not found");
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
-router.delete("/:contactId", async (req, res, next) => {
-  const result = await contacts.removeContactById();
-  res.json(result);
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      throw CustomError(404, error.message);
+    }
+    const { id } = req.params;
+    const result = await contacts.updateContactById(id, req.body);
+    if (!result) {
+      throw CustomError(404, "Not Found");
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
-router.put("/:contactId", async (req, res, next) => {
-  const result = await contacts.updateContactById();
-  res.json(result);
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contacts.removeContactById(id);
+    if (!result) {
+      throw CustomError(404, "Not Found");
+    }
+    res.json({
+      message: "Contact deleted successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-
-app.listen(3000, () => {
-  console.log("Server is running. Use our Api on port: 3000");
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running. Use our Api on port: ${PORT}`);
 });
